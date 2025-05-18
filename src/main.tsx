b0300d685e06db3fd8e5777129d7c36d406b4f97
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-05-19 00:22:55 +0400

    global provider added

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GlobalProvider } from './Context/GlobalContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-
+<GlobalProvider>
     <App />
+    </GlobalProvider>
 
 )
